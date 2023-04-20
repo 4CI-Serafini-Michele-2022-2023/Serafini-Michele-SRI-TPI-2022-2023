5d8cef12204207e0caa119a3a5c4ed8c6347cb7a
Michele Serafini <mikesera@gmail.com>
2023-04-20 19:13:11 +0200

    SIUM

--- AI/package/AI.py
+++ AI/package/AI.py
@@ -1,11 +1,13 @@
 from pandas import read_csv
-from sklearn import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier
+from icecream import ic
 
 giocatori = read_csv('../flussi/giocatori.csv')
 inp = giocatori.drop(columns=['videogame'])
 out = giocatori['videogame']
 modello = DecisionTreeClassifier()
 modello.fit(inp.values, out.values)
-età=input("Inserisci l'età della persona")
-previsione = modello.predict([[0,età]])
-print(previsione)
+età=int(input("Inserisci l'età della persona"))
+genere=int(input("Dammi il genere"))
+previsione = modello.predict([[genere,età]])
+ic(previsione)
