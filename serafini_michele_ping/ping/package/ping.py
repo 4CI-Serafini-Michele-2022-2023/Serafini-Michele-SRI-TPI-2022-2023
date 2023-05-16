4b4ef67811651a369eea77c3b0ec3c82210b4173
Michele Serafini <mikesera05@gmail.com>
2023-05-16 12:41:39 +0200

    top

--- serafini_michele_ping/ping/package/ping.py
+++ serafini_michele_ping/ping/package/ping.py
@@ -1,17 +1,48 @@
+import csv
 import os
+import platform
 import subprocess
-from pandas import read_csv
+import threading
 
-def pingw():
-
-
-
-def pingl():
-
-    
-if __name__=="__main__":
-    file = read_csv("input.csv")
-    if platform == "win32":
-        pingw()
+def ping_host(host):
+    if platform.system() == 'Windows':
+        command = ['ping', '-n', '1', host]
+    elif platform.system() == 'Linux':
+        command = ['ping', '-c', '1', host]
     else:
-        pingl()
+        print(f"Sistema operativo non supportato per l'host {host}")
+        return
+
+    result = subprocess.run(command, capture_output=True)
+    output = result.stdout.decode('utf-8')
+    if platform.system() == 'Windows' and "Risposta" in output:
+        print(f"{host} è raggiungibile.")
+    elif platform.system() == 'Linux' and "1 received" in output:
+        print(f"{host} è raggiungibile.")
+    else:
+        print(f"{host} non è raggiungibile.")
+
+def ping_hosts(hosts):
+    threads = []
+    for host in hosts:
+        thread = threading.Thread(target=ping_host, args=(host,))
+        thread.start()
+        threads.append(thread)
+
+    for thread in threads:
+        thread.join()
+
+def main():
+    csv_file = 'input.csv'  # Specifica il percorso del tuo file CSV
+    hosts = []
+
+    with open(csv_file, 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            if len(row) > 0:
+                hosts.append(row[0])
+
+    ping_hosts(hosts)
+
+if __name__ == '__main__':
+    main()
